950df600340040bfa2cac1331b6782dc8271753b
Vincent Acanfora <brodino96@gmail.com>
2024-12-13 08:56:21 +0100

    Added debug

--- server/database.lua
+++ server/database.lua
@@ -3,7 +3,13 @@
 ---@return table
 ---@param id number|string The serverId of the player
 function FetchTags(id)
+
     ---@diagnostic disable-next-line: undefined-global
     local tags = MySQL.scalar.await("SELECT `tags` FROM `users` WHERE `identifier` = ? LIMIT 1", { ESX.GetPlayerFromId(id).getIdentifier() })
+
+    if not tags then
+        Debug.error("Unable to fetch the tags for id ["..id.."]", true)
+        return {}
+    end
     return tags
 end
