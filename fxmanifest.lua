694dd6edc2841ea5f7c3f81984455b73fba71dcd
Vincent Acanfora <brodino96@gmail.com>
2024-12-13 15:27:43 +0100

    added config.lua to shared_scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ author "Brodino"
 version "1.0"
 description "Tag, you're it!"
 
-shared_scripts { "@ox_lib/init.lua", "shared.lua", }
+shared_scripts { "@ox_lib/init.lua", "config.lua", "shared.lua", }
 server_scripts { "@oxmysql/lib/MySQL.lua", "server/*", }
 client_scripts { "client.lua", }
 
