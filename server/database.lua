604bfe51208bac597754859885e86d9f6c469f7a
Vincent Acanfora <brodino96@gmail.com>
2024-12-13 10:24:33 +0100

    Added: function to updated database

--- server/database.lua
+++ server/database.lua
@@ -8,8 +8,29 @@ function FetchTags(id)
     local tags = MySQL.scalar.await("SELECT `tags` FROM `users` WHERE `identifier` = ? LIMIT 1", { ESX.GetPlayerFromId(id).getIdentifier() })
 
     if not tags then
-        Debug.error("Unable to fetch the tags for id ["..id.."]", true)
+        Debug.error("Function 'FetchTags': unable to return the tags for id ["..id.."]", true, debug.getinfo(1).currentline)
         return {}
     end
     return json.decode(tags)
+end
+
+---Updated the database with the new tags
+---@param playerId integer Player's id
+---@return nil
+function UpdateTags(playerId)
+
+    if not Tags[playerId] then
+        return Debug.error("Function 'UpdateTags': the specified player didn't have any locally saved tags", true, debug.getinfo(1).currentline)
+    end
+
+    ---@diagnostic disable-next-line: undefined-global
+    local response = MySQL.update.await("UPDATE users SET tags = ? WHERE identifier = ?", {
+        Tags[playerId], ESX.GetPlayerFromId(playerId).getIdentifier()
+    })
+
+    if not response then
+        return Debug.error("Function 'UpdateTags': no response was given from the database", true, debug.getinfo(1).currentline)
+    end
+
+    SyncTags(playerId)
 end
