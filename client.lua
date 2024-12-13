426305b179d95689adb9b99ea8e0855d734a2bf9
Vincent Acanfora <brodino96@gmail.com>
2024-12-13 10:23:34 +0100

    Added: function to fetch tags, debugs, sync events

--- client.lua
+++ client.lua
@@ -1,18 +1,39 @@
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
 Tags = {}
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
+---Returns the list of all the player tags
+---@return table
+local function fetchTags()
+    ---@diagnostic disable-next-line: undefined-global
+    return lib.callback.await("br_tags:getTags")
+end
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
 
 ---Returns the list of all the tags for this player
 ---@return table
 local function getTags()
+    if not Tags then
+        Debug.info("Function: 'getTags': didn't have any tag stored locally, fetching from database", false, debug.getinfo(1).currentline)
+        Tags = fetchTags()
+    end
     return Tags
 end
 
----Returns if the player has the specified tag
+---Returns true if the player has the specified tag
 ---@return boolean|nil
 ---@param name string Name of the tag
 local function hasTag(name)
 
     if not Shared.type(name, "string") then
-        return
+        return Debug.error("Function: 'hasTag': tag name should be a string, parameter with type ["..type(name).."] was passed", true, debug.getinfo(1).currentline)
+    end
+
+    if not Tags then
+        Tags = fetchTags()
     end
 
     for i = 1, #Tags do
@@ -20,8 +41,20 @@ local function hasTag(name)
             return true
         end
     end
+
     return false
 end
 
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
+RegisterNetEvent("br_tags:syncTags")
+AddEventHandler("br_tags:syncTags", function (newTags)
+    Tags = newTags
+end)
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
 exports("getTags", getTags)
 exports("hasTag", hasTag)
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
