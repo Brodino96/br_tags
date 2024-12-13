f21aae2d84a763cfc5f7ce77fabcc0daa3c7802d
Vincent Acanfora <brodino96@gmail.com>
2024-12-13 09:18:41 +0100

    Added dependencies

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version "cerulean"
 games { "gta5" }
 lua54 "yes"
 
-shared_scripts { "shared.lua", }
+shared_scripts { "@ox_lib/init.lua", "shared.lua", }
 server_scripts { "@oxmysql/lib/MySQL.lua", "server.lua", }
 client_scripts { "client.lua", }
+
+dependencies { "ox_lib", "es_extended" }
