007c92517b427768c0000fc2735cc8fb902bfe4e
Brodino <brodino96@gmail.com>
2024-12-14 16:04:42 +0100

    Reformatted Shared.type

--- shared.lua
+++ shared.lua
@@ -2,27 +2,18 @@ Shared = {
     ---Checks the type of the first variable and if it matches the second it returns true
     ---@return boolean
     ---@param variable any The variable that have to match the type
-    ---@param targetType string|table The tag or list of tags that have to match
-    type = function (variable, targetType)
-        local t = type(variable)
+    ---@param targetType string The tag or list of tags that have to match
+    ---@param functionName string The name of the function called
+    type = function (variable, targetType, functionName)
+        local match = type(variable)
 
-        if type(targetType) == "table" then
-            for i = 1, #targetType do
-                if targetType[i] == t then
-                    return true
-                end
-            end
-            print("Was expecting a string but recived type: "..t)
-            return false
-
-        else
-            if t == targetType then
-                return true
-            else
-                print("Was expecting a string but recived type: "..t)
-                return false
-            end
+        if match == targetType then
+            return true
         end
+
+        Debug.error(string.format("Function: '%s': was expecting [%s] but got [%s]", functionName, targetType, match), true)
+
+        return false
     end
 }
 
@@ -30,28 +21,31 @@ Debug = {
     ---@return nil
     ---@param text string|number The text that needs to be printed
     ---@param bypass boolean If Config.debug should be bypassed
-    ---@param line integer Current line of code
+    ---@param line integer|nil Current line of code
     success = function (text, bypass, line)
         if Config.debug or bypass then
-            return print("[^2SUCCESS at line ^0"..line.."] "..tostring(text))
+            return print(string.format("%d [^2SUCCESS^0] %s", line, tostring(text)))
+            --return print("[^2SUCCESS at line ^0"..line.."] "..tostring(text))
         end
     end,
     ---@return nil
     ---@param text string|number The text that needs to be printed
     ---@param bypass boolean If Config.debug should be bypassed
-    ---@param line integer Current line of code
+    ---@param line integer|nil Current line of code
     info = function (text, bypass, line)
         if Config.debug or bypass then
-            return print("[^3INFO at line ^0"..line.."] "..tostring(text))
+            return print(string.format("%d [^3INFO^0] %s", line, tostring(text)))
+            --return print("[^3INFO at line ^0"..line.."] "..tostring(text))
         end
     end,
     ---@return nil
     ---@param text string|number The text that needs to be printed
     ---@param bypass boolean If Config.debug should be bypassed
-    ---@param line integer Current line of code
+    ---@param line integer|nil Current line of code
     error = function (text, bypass, line)
         if Config.debug or bypass then
-            return print("[^1ERROR at line ^0"..line.."] "..tostring(text))
+            return print(string.format("%d [^1ERROR^0] %s", line, tostring(text)))
+            --return print("[^1ERROR at line ^0"..line.."] "..tostring(text))
         end
     end
 }
