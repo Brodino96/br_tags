43a45f2e2ecede813acf5ff2521e5c61c1a53205
Vincent Acanfora <brodino96@gmail.com>
2025-01-08 16:08:33 +0100

    initialized the fronted part of the script

---
+++ client/nui.lua
@@ -0,0 +1,36 @@
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
+local function openMenu()
+    local pList = lib.callback.await("br_tags:getOnlinePlayers")
+    SendNUIMessage({
+        action = "open",
+        players = pList
+    })
+    SetNuiFocus(true, true)
+end
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
+RegisterNUICallback("close", function (body, cb)
+    SetNuiFocus(false, false)
+    cb()
+end)
+
+RegisterNUICallback("selectPlayer", function (body, cb)
+    cb(lib.callback.await("br_tags:getUserInfo", false, body.identifier))
+end)
+
+RegisterNUICallback("search", function (body, cb)
+    cb(lib.callback.await("br_tags:searchPlayer", false, body.name))
+end)
+
+RegisterNUICallback("onlineList", function (body, cb)
+    cb(lib.callback.await("br_tags:getOnlinePlayers"))
+end)
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
+
+RegisterNetEvent("br_tags:openMenu")
+AddEventHandler("br_tags:openMenu", openMenu)
+
+------------------------ # ------------------------ # ------------------------ # ------------------------ # ------------------------
