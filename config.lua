6bba5eae837f05ff072077dae352bd4e34f98bc7
Vincent Acanfora <brodino96@gmail.com>
2025-01-08 17:00:23 +0100

    updated: better comments

--- config.lua
+++ config.lua
@@ -1,14 +1,17 @@
 Config = {
 
-    -- A list of all the tags that can be assigned to players
-    allowedTags = { "admin", "user", "brodino" },
+    --[[
+    * A list of all the tags that can be assigned to players
+    * Leave the table empty if you want to allow every word
+    ]]
+    allowedTags = { "user", "admin", "brodino" },
 
     --[[
     * When `keepRemovedTags` is set to `true` you can give a tag to a player, then remove that tag from this config
         and the player will still posses that tag, if you instead set the config to `false` the script will check
         the player tags when he connects to the server and then remove the deleted ones
     ]]
-    keepRemovedTags = true,
+    keepRemovedTags = false,
 
     -- Prints all the actions in the console (use when encountering a problem)
     debug = true,
