2b47b69ffc4feb7c6080daeba6e7d5f59e85f00c
Vincent Acanfora <brodino96@gmail.com>
2025-01-08 10:35:54 +0100

    removed: MySQL diagnostics added: project wide MySQL definition

--- server/database.lua
+++ server/database.lua
@@ -4,7 +4,6 @@
 ---@return table|nil
 function FetchTags(id)
 
-    ---@diagnostic disable-next-line: undefined-global
     local tags = MySQL.scalar.await("SELECT `br_tags` FROM `users` WHERE `identifier` = ? LIMIT 1", { ESX.GetPlayerFromId(id).getIdentifier() })
 
     if not tags then
@@ -22,7 +21,6 @@ function UpdateTags(playerId)
         return Debug.error("Function 'UpdateTags': the specified player didn't have any locally saved tags", true, debug.getinfo(1).currentline)
     end
 
-    ---@diagnostic disable-next-line: undefined-global
     local response = MySQL.update.await("UPDATE users SET br_tags = ? WHERE identifier = ?", {
         json.encode(Tags[playerId]), ESX.GetPlayerFromId(playerId).getIdentifier()
     })
@@ -32,4 +30,21 @@ function UpdateTags(playerId)
     end
 
     SyncTags(playerId)
+end
+
+---Fetches the user info to be used in the menu
+---@param identifier string The player identifier saved in the database
+---@return table
+function FetchUserInfo(identifier)
+
+    local serverId = ESX.GetPlayerFromIdentifier(identifier)
+    if Tags[serverId] then
+        return Tags[serverId]
+    end
+
+    local info = MySQL.single.await("SELECT `firstname`, `lastname`, `group`, `job`, `job_grade`, `dateofbirth`, `tags` FROM `users` WHERE `identifier` = ? LIMIT 1", {
+        identifier
+    })
+
+    return info
 end
